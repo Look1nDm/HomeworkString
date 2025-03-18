a055e373fd74660c0a21823d89b0b4ea757b7b7e
Senina OS <karolga93@mail.ru>
2025-03-18 12:27:07 +0300

    Задачи 1 и 2 Задача 3 решена только для данной строки - не учитываются двузначные римские цифры. Пока думаю над решением

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.sql.Array;
+import java.sql.Struct;
 import java.util.Arrays;
 
 public class Main {
@@ -18,12 +20,16 @@ public class Main {
 
         // для первого задания
         System.out.println(Arrays.toString(reverse(example1.toCharArray())));
+        String ex1 = new String(reverse(example1.toCharArray())); //хочется строкой вывести, не массивом=)
+        System.out.println(ex1);
 
         // вывод на экран ответа второго задания
         System.out.println(merge(example2, example3));
+
         // вывод ответа третьего
         System.out.println(romanToInt("MMXXV"));
     }
+
     /*
     В этом методе нужно написать алгоритм переворота строки в обратном порядке
     тоже самое, что делали с массивом чисел, просто перевернуть фразы
@@ -31,8 +37,11 @@ public class Main {
     И возвращет тоже не строку (за забудьте при выводе)
      */
     static char[] reverse(char[] s) {
-        // писать логику тут
-        return null;
+        char[] result = new char[s.length];
+        for (int i = 0; i < s.length; i++) {
+            result[i] = s[(s.length - 1) - i];
+        }
+        return result;
     }
     // в этой задачке необходимо составить строку из двух слов в такой последовательности:
     // первая буква первого слова будет первой в новой строке
@@ -41,11 +50,39 @@ public class Main {
     // вторая буква второго слова будет четвертой в новой строке
     // и т.д.
     // пример строка1 = абс, строка2 = про -> ответ апбрсо
+
     static String merge(String word1, String word2) {
+
+        //я тут решила немного унифицировать, чтобы задача подходила к строкам даже сильно разной длины.
+        //после того, как кончилась более короткая - просто вписываю дальше длинную подряд
         int i = 0;
+        int minLenght;
+        boolean is1Longer;
+        if (word1.length() < word2.length()) {
+            minLenght = word1.length();
+            is1Longer = false;
+        } else {
+            minLenght = word2.length();
+            is1Longer = true;
+        }
+
         StringBuilder merge = new StringBuilder(); // <- посмотрите основные методы этого класса, из них потребуется использовать один
-        // писать логику тут
-        return null;
+        do {
+            merge.append(word1.charAt(i));
+            merge.append(word2.charAt(i));
+            i++;
+        } while (i < minLenght);
+
+        if (is1Longer) {
+            for (int j = word2.length(); j < word1.length(); j++) {
+                merge.append(word1.charAt(j));
+            }
+        } else {
+            for (int j = word1.length(); j < word2.length(); j++) {
+                merge.append(word2.charAt(j));
+            }
+        }
+        return merge.toString();
     }
 
     /*
@@ -55,10 +92,22 @@ public class Main {
     Желаю удачи, Войны!
      */
     static int romanToInt(String s) {
-        int[] arabicNumbers = {0,1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        int[] arabicNumbers = {0, 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
         String[] romanNumbers = {"", "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
         int result = 0;
-        // тут писать саму логику
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = (romanNumbers.length - 1); j > 0; ) {
+                if (s.contains(romanNumbers[j])) {
+                    result += arabicNumbers[j];
+                    s = s.substring(s.indexOf(romanNumbers[j]) + 1);
+                    i = 0;
+                } else {
+                    j--;
+                }
+            }
+        }
         return result;
     }
+
 }
